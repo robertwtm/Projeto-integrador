d2564b3e3c2e4cff49f6ffef94c9950239ec845d
Augusto Cardoso Xavier <augustocx15@gmail.com>
2021-12-06 09:13:35 -0300

    Subindo os arquivos do Spring

---
+++ src/main/java/com/generation/AppProjetoIntegrador/AppProjetoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.generation.AppProjetoIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProjetoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppProjetoIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/AppProjetoIntegrador/AppProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.AppProjetoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
