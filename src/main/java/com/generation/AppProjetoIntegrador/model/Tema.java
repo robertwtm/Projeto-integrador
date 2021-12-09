73443eef0bd26b46a87c699ef87a0f726daa9746
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-09 10:02:51 -0300

    @robertwtm task atualizando a model

--- src/main/java/com/generation/AppProjetoIntegrador/model/Tema.java
+++ src/main/java/com/generation/AppProjetoIntegrador/model/Tema.java
@@ -1,9 +1,13 @@
 package com.generation.AppProjetoIntegrador.model;
 
+import java.util.List;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_tema")
@@ -11,20 +15,65 @@ public class Tema {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id_tema;
+	private long idTema;
 	
 	@NotNull
  	@Size(min = 5, max = 45)
 	@NotBlank
 	private String materia;
 	
+	@NotNull
  	@Size(min = 5, max = 100)
+	@NotBlank
 	private String descricao;
  	
+	@NotNull
  	@Size(min = 5, max = 100)
+	@NotBlank
 	private String url_imagem;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Usuario> usuario;
+	
+ 	//GET E SET
+	public String getMateria() {
+		return materia;
+	}
 
+	public long getIdTema() {
+		return idTema;
+	}
 
+	public void setIdTema(long idTema) {
+		this.idTema = idTema;
+	}
 
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
 
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUrl_imagem() {
+		return url_imagem;
+	}
+
+	public void setUrl_imagem(String url_imagem) {
+		this.url_imagem = url_imagem;
+	}
+
+	public List<Usuario> getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(List<Usuario> usuario) {
+		this.usuario = usuario;
+	}
 }
