999b90b7345dcb2e7aa9f46f43adc11f9a267613
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2022-02-01 12:32:12 -0300

    Update Postagem.java

--- src/main/java/com/generation/AppProjetoIntegrador/model/Postagem.java
+++ src/main/java/com/generation/AppProjetoIntegrador/model/Postagem.java
@@ -22,12 +22,12 @@ public class Postagem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank(message = "O atributo titulo é Obrigatório!")
-	@Size(min = 5, max = 100, message = "O atributo titulo deve ter no mínimo 5 e no máximo 100 caracteres")
+	//@NotBlank(message = "O atributo titulo é Obrigatório!")
+	//@Size(min = 5, max = 100, message = "O atributo titulo deve ter no mínimo 5 e no máximo 100 caracteres")
 	private String titulo;
 
 	@NotNull(message = "O atributo texto é Obrigatório!")
-	@Size(min = 10, max = 1000, message = "O atributo texto deve ter no mínimo 10 e no máximo 1000 caracteres")
+	@Size(min = 3, max = 1000, message = "O atributo texto deve ter no mínimo 3 e no máximo 1000 caracteres")
 	private String texto;
 
 	@UpdateTimestamp
